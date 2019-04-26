180e79581e393adb04efaaa4b0d417330a74b265
Michael Cue <MichaelCue@Michaels-MacBook-Pro-2.local>
2019-04-26 16:59:58 -0400

    updated connection in server.js

--- server.js
+++ server.js
@@ -5,7 +5,24 @@ const app = express()
 const methodOverride = require('method-override')
 const logger = require('morgan')
 const mainUserApi = require('./api/mainUser.js')
+const mongoose = require('mongoose');
 
+
+  if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  }
+  else {
+    mongoose.connect('mongodb://localhost/Project2',{useNewUrlParser:true});
+    
+  }
+  mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+  });
 
 //============Middleware================
 
