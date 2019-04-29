db741495299a7862b4f3212c8c249831fa1822ac
Michael Cue <MichaelCue@Michaels-MacBook-Pro-2.local>
2019-04-29 09:38:18 -0400

    Changed small detail on main Api

--- api/mainUser.js
+++ api/mainUser.js
@@ -3,8 +3,8 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const LibrarySchema = mongoose.Schema({
     name: String,
-    bookName: String,
-    gameName: String,
+    BooksRented: Number,
+    MoviesRented: Number,
     userId: ObjectId
 })
 
