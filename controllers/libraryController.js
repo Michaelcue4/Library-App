b00ac376e71af3971c513e9fa0828c1928309bee
Michael Cue <MichaelCue@Michaels-MacBook-Pro-2.local>
2019-04-30 18:16:18 -0400

    dir Cleanup

--- controllers/libraryController.js
+++ controllers/libraryController.js
@@ -5,7 +5,10 @@ const libraryController =
     index:(req,res)=>
     Library.find({}).then(library=>{
         res.render('library/index',{library})
-    })
+    }),
+    new:(req,res)=>{
+        res.render('library/new')
+    }
 
 }
 
