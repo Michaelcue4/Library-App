3eb989d8cba97a1628f6988196b7a4cd3a64f0b1
Michael Cue <MichaelCue@Michaels-MacBook-Pro-2.local>
2019-05-01 12:27:02 -0400

    small errors fixed

--- controllers/libraryController.js
+++ controllers/libraryController.js
@@ -11,12 +11,9 @@ const libraryController =
         res.render('library/new')
     },
     create:(req,res)=>{
-        Library.create({
-            userName:req.body.userName,
-            isActive:req.body.isActive
-        }).then(newlibrary=>{
-            res.redirect('/')
-        })
+        Library.create(req.body).then(() => {
+            res.redirect("/");
+          })
     },
     show:(req,res)=>{
         const libraryId = req.params.id 
@@ -25,10 +22,14 @@ const libraryController =
         })
     },
     edit:(req,res)=>{
-        
+        Library.findById(req.params.id).then(Singlelibrary=>{
+            res.render('library/edit', {Singlelibrary})
+        })
     },
     update:(req,res)=>{
-
+        Library.updateOne({_id : req.params.id}, req.body).then(() => {
+            res.redirect("/" + req.params.id);
+          })
     },
     delete:(req,res)=>{
         Library.deleteOne({"_id":req.params.id}).then(()=>{
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,11 @@ const onHoldController = require('../controllers/onHoldController')
 //router.use(express.static('frontEnd'))
 ///////////////////////////////////////////////////
 router.get('/', libraryController.index)
+router.get('/new', libraryController.new);
 router.get('/:id', libraryController.show);
+router.get('/:id/edit', libraryController.edit);
 
 router.post('/', libraryController.create);
+router.put('/:id', libraryController.update);
 router.delete('/:id', libraryController.delete);
 module.exports = router;
